00b76c60fa02d93ad738a0c11732f5302b9cf964
ArlindPestisha <arlind.pestisha@hyperisland.se>
2021-05-12 18:59:57 +0200

    adding styled components and config

---
+++ components/NavBar.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  background: black;
+  height: 80px;
+`
+
+
+const NavBar = () => {
+  return (
+    <Nav>
+      <h1>Nav</h1>
+    </Nav>
+   );
+}
+ 
+export default NavBar;
